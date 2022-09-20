648a03902ef41ba8943d189436baa9d9643f1569
kapil <kapil@DESKTOP-IQGFFDT>
2022-09-20 15:46:45 +0530

    try commiting

--- src/DemoLoop/DemoForLoop.java
+++ src/DemoLoop/DemoForLoop.java
@@ -5,7 +5,7 @@ public class DemoForLoop {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i=1;i<=5;i++) {
-			for(int j=i;j>=1;j--) {
+			for(int j=1;j<=5;j++) {
 				System.out.print(j+" ");
 			}
 			System.out.println();
